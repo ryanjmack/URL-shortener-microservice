5945eee7c4c91603787c32a390760a51f0e57a67
root <ryanjmack21@gmail.com>
2018-08-28 18:21:37 -0400

    Setup route for POST request

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const PORT    = Number(process.env.PORT || 3000);
 const express = require('express');
 const app     = express();
 
+// bodyParser for POST requests
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// middleware for serving static files
 app.use(express.static(__dirname + '/public'));
 
 
@@ -15,6 +20,13 @@ app.get('/api/shorturl/:id?', (req, res) => {
   // TODO: query DB with id of the shorturl and redirect the user to it
   console.log(req.params.id);
   res.sendFile('index.html', {root: __dirname + '/public'});
+});
+
+
+app.post('/api/shorturl/new', (req, res) => {
+  const postBody = req.body.url;
+    // Return the POST message
+  res.send(postBody);
 });
 
 
